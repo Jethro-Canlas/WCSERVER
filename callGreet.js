84a91976778cff784430d40c36fcab250cb7bef7
Jethro-Canlas <jethrocanlas7@gmail.com>
2025-07-07 11:51:18 +0800

    Delete callGreet.js

--- callGreet.js
+++
@@ -1,7 +0,0 @@
-var greeting = require('./greet.js');
-var greeting1 = new greeting('Messmer');
-var greeting2 = new greeting('John');
-var greeting3 = new greeting('Jane');
-console.log(greeting1.greeting());
-console.log(greeting2.greeting());
-console.log(greeting3.greeting());
